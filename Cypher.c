b2cdf20ce78ab5d047f6f13c1276d820de4e318e
Miguel <mangelsr@live.com>
2017-05-18 16:14:27 -0500

    Inicio de proyecto

---
+++ Cypher.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+//IMPORTANTE: Revisar tabla assci
+//se puede usar la libreria string.h
+/*
+A: 65
+Z: 90
+
+a: 97
+z: 122
+*/
+
+int main(int argc, char **argv){
+	printf("Cifrado ciclico");
+	printf("Ingrese el mensaje a cifrar: ");
+	printf("Ingrese la llave numerica: ")
+	printf("Mensaje cifrado: ")
+	return 0;
+}
+
+char cifrar(char mensaje,int llave){
+	
+}
